5701cdc91510dd18bc90030ed14cb1e98d988ecd
Amayuru1999 <amarasingheau@gmail.com>
2024-06-03 16:14:54 +0530

    User Respond Issue Solved

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserRequest } from './dto/create-user-request.dto';
+import { CreateUserRequest } from './dto/request/create-user-request.dto';
 import { UsersRepository } from './users.repository';
+import { hash } from 'bcrypt';
+import { UserResponse } from './dto/response/user-response.dto';
+import { User } from './models/User';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class UsersService {
     constructor(private readonly usersRepository:UsersRepository){}
 
-    async createUser(createUserRequest:CreateUserRequest):Promise<any>{
-        return this.usersRepository.insertOne(createUserRequest)
+    async createUser(createUserRequest:CreateUserRequest):Promise<UserResponse>{
+        const user = await this.usersRepository.insertOne({
+            ...createUserRequest,
+            password: await hash(createUserRequest.password,10),
+        });
+        return this.buildResponse(user);
+    }
+
+    private buildResponse(user:User):UserResponse{
+        return{
+            _id:(user._id as Types.ObjectId).toHexString(),
+            email:user.email,
+        };
     }
 }
